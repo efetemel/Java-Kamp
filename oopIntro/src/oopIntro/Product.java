5094edc2fbdd24487719b7793a9e57af9a6c7994
efetemelers <ersozyazilim@gmail.com>
2021-04-24 23:19:34 +0300

    2.Gün Ders

--- oopIntro/src/oopIntro/Product.java
+++ oopIntro/src/oopIntro/Product.java
@@ -2,6 +2,17 @@ package oopIntro;
 
 public class Product {
 	
+	public Product() {
+		System.out.println("Ben çalıştım");
+	}
+	
+	public Product(int id,String name,double unitPrice,String detail) {
+		this.id = id;
+		this.name = name;
+		this.unitPrice = unitPrice;
+		this.detail = detail;
+	}
+	
 	int id;
 	String name;
 	double unitPrice;
